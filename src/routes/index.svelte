fe86385c7988cc3db480db73579d3d48160d2b05
trevor <me@trevorbehnke.com>
2021-10-02 12:57:44 -0700

    init commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<!-- App.svelte -->
+<script>
+	import Todos from '$lib/components/Todos.svelte';
+	import Alert from '$lib/components/Alert.svelte';
+	import { todos } from '../stores.js';
+</script>
+
+<Alert />
+<Todos bind:todos={$todos} />
+
+<!-- <script context="module">
+	export const ssr = false;
+	import { todos } from '../stores/stores';
+</script>
+
+<main>
+	{#each $todos as { id, name, completed }}
+		<section>
+			{id}: {name} - {completed}
+		</section>
+	{/each}
+</main> -->
